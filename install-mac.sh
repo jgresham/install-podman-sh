b3c70bf63d6581b9de8ebb65b8f1b92425e5c243
jgresham <johnsgresham@gmail.com>
2023-02-20 16:27:19 -0800

    add simple mac testuname -m

---
+++ install-mac.sh
@@ -0,0 +1,4 @@
+curl -LO https://github.com/containers/podman/releases/download/v4.4.1/podman-installer-macos-arm64.pkg
+
+# Install Podman
+sudo installer -pkg podman-installer-macos-arm64.pkg -target ~/ -verbose
