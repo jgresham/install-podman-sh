d345173d5592fe82a7842a23e282bd1c9b80480a
jgresham <johnsgresham@gmail.com>
2023-02-20 19:56:04 -0800

    verbose rm commands for mac

--- install-mac.sh
+++ install-mac.sh
@@ -1,12 +1,10 @@
 # remove any previously downloaded installer
-rm podman-installer*.pkg
+rm -v podman-installer*.pkg
+# remove previous installation config (only do for fresh install)
 # https://stackoverflow.com/questions/72562644/podman-on-mac-throws-error-when-running-podman-machine-init/72571826#72571826
-rm -rf ~/.config/containers/
-rm -rf ~/.local/share/containers
-rm ~/.ssh/podman*
-
-
-echo $PATH
+rm -rfv ~/.config/containers/
+rm -rfv ~/.local/share/containers
+rm -v ~/.ssh/podman*
 
 # Check if the macOS system is running on arm64 architecture
 if [[ $(uname -m) == "arm64" ]]; then
@@ -21,17 +19,6 @@ fi
 
 # Install Podman (installs at /opt/podman)
 sudo installer -pkg podman-installer-macos*.pkg -target / -verbose
-# ls /opt/podman/bin
-# export PATH="/opt/podman/bin:$PATH"
-# echo $PATH
-
-# podman version
-podman machine init
-podman machine start
-# podman version
-
-# source ~/.bash_profile
-# echo $PATH
 
 # Cleanup
 rm podman-installer*.pkg
