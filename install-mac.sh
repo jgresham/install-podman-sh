5ba5f1a82a2f1536f76925bceb6669eacf9061d4
jgresham <johnsgresham@gmail.com>
2023-12-18 18:05:54 -0600

    feat: perf test mac

--- install-mac.sh
+++ install-mac.sh
@@ -6,15 +6,17 @@ rm -rfv ~/.config/containers/
 rm -rfv ~/.local/share/containers
 rm -v ~/.ssh/podman*
 
+PODMAN_VERSION=4.8.2
+
 # Check if the macOS system is running on arm64 architecture
 if [[ $(uname -m) == "arm64" ]]; then
   echo "Downloading arm64 Podman"
   # Download the latest Podman release for macOS arm64
-  curl -LO https://github.com/containers/podman/releases/download/v4.4.2/podman-installer-macos-arm64.pkg
+  curl -LO https://github.com/containers/podman/releases/download/"$PODMAN_VERSION"/podman-installer-macos-arm64.pkg
 else
   echo "Downloading x86_64 Podman"
   # Download the latest Podman release for macOS x86_64
-  curl -LO https://github.com/containers/podman/releases/download/v4.4.2/podman-installer-macos-amd64.pkg
+  curl -LO https://github.com/containers/podman/releases/download/"$PODMAN_VERSION"/podman-installer-macos-amd64.pkg
 fi
 
 # Install Podman (installs at /opt/podman)
